dc24ca1328dd97aec24628dbff95bcc24360bfe2
JhojanDev <102551448+JhojanBinary@users.noreply.github.com>
2024-06-13 16:58:45 +0000

    Register

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,11 +5,13 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import { Register } from "./pages/Register";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+
 
 //create your first component
 const Layout = () => {
@@ -25,6 +27,7 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
+                        <Route element={<Register></Register>}  path="/register"/>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
