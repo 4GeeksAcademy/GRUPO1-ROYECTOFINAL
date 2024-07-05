3067fbca695e09a3ef011f28adaa62da1ce80942
Agustin Trezza <88919386+Agustintrezza@users.noreply.github.com>
2024-07-05 04:38:30 +0000

    6to

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
+        path: path.resolve(__dirname, 'public'), // Asegúrate de que los archivos se generen en 'public'
+        filename: 'bundle.js',
         publicPath: '/'
     },
     plugins: [
